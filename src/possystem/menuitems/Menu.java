5197704befff6e15a92b4bd4f7e54c8397deff61
DaKingofCheckerz <mario7292000@gmail.com>
2021-11-17 03:35:23 -0500

    Began working on Menu class

---
+++ src/possystem/menuitems/Menu.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package possystem.menuitems;
+
+import java.util.Map;
+import java.util.UUID;
+
+/**
+ * Class for storing the entire menu. Also stores
+ *
+ * @author dakingofcheckerz
+ */
+public class Menu {
+
+    private Map<UUID, MenuItem> menuMap;
+}
