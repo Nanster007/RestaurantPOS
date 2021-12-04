d22ff40ce1172852127c74e69d968bb2fafafff4
DaKingofCheckerz <mario7292000@gmail.com>
2021-12-04 18:48:39 -0500

    Minor code formatting changes

--- src/possystem/ClockThread.java
+++ src/possystem/ClockThread.java
@@ -3,23 +3,23 @@ package possystem;
 import java.util.Date;
 import javax.swing.JPanel;
 
-public class ClockThread extends Thread{
-    
+public class ClockThread extends Thread {
+
     private CustomPanel currentPanel;
     private String time;
-    
-    public ClockThread(CustomPanel currentPanel){
+
+    public ClockThread(CustomPanel currentPanel) {
         this.currentPanel = currentPanel;
         start();
     }
-    
-    public void run(){
-        while(true){
+
+    public void run() {
+        while (true) {
             time = new Date().toString();
             currentPanel.setClock(time);
         }
     }
-    
+
     public CustomPanel getCurrentPanel() {
         return currentPanel;
     }
