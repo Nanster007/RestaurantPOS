0af0303456860dc151e87e8d5d5121ea4e643d4f
DaKingofCheckerz <mario7292000@gmail.com>
2021-12-05 04:57:37 -0500

    Added count member variable

--- src/possystem/menuitems/Topping.java
+++ src/possystem/menuitems/Topping.java
@@ -14,16 +14,36 @@ import java.util.UUID;
  */
 public class Topping extends MenuItem {
 
+    private int count;
     private int maxCount;
     // Could add Topping option support at some point
 
     public Topping(String name, double price, UUID id, String comments, String toppingCategory, int maxCount) {
         super(name, price, id, null, null, comments, "Topping", toppingCategory);
 
+        this.count = 0;
         this.maxCount = maxCount;
+    }
+
+    public void setCount(int count) {
+        if (count >= 0 && count <= maxCount) {
+            this.count = count;
+        }
+    }
+
+    public int getCount() {
+        return this.count;
     }
 
     public int getMaxCount() {
         return this.maxCount;
+    }
+
+    @Override
+    public String toString() {
+        String toString = basePrice * count + " \n" + name + '(' + count + ')'
+                + "\n" + comments;
+
+        return toString;
     }
 }
